04cd7049c5eeee3dcd1156140dc3b8f236e97faf
isaac <iwvelando@gmail.com>
2013-08-10 18:43:01 -0500

    Minor icon dictionary change

--- wunderground_current.py
+++ wunderground_current.py
@@ -5,7 +5,7 @@ import os
 
 # Extract command line arguments and grab the wunderground API key
 location = sys.argv[1]
-apiKey = open(os.getenv('HOME') + '/.api/wunderground','r').readline().split('\n')[0]
+apiKey = open(os.getenv('HOME') + '/.api/wunderground', 'r').readline().split('\n')[0]
 
 # Wunderground API call for current conditions data
 f = urllib2.urlopen('http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + location + '.json')
@@ -15,26 +15,28 @@ parsed_json = json.loads(json_string)
 current = parsed_json['current_observation']
 
 #Set a dictionary for parsing the icon field to a smaller text
-icons = {'chanceflurries' : 'flurry?',
-	 'chancerain' : 'rain?',
-	 'chancesleet' : 'sleet?',
-	 'chancesnow' : 'snow?',
-	 'chancetstorms' : 'tstorm?',
-	 'clear' : 'clear',
-	 'cloudy' : 'cloudy',
-	 'flurries' : 'flurry',
-	 'fog' : 'fog',
-	 'hazy' : 'hazy',
-	 'mostlycloudy' : 'cloudy',
-	 'mostlysunny' : 'sunny',
-	 'partlycloudy' : 'cloudy~',
-	 'partlysunny' : 'sunny~',
-	 'rain' : 'rain',
-	 'sleet' : 'sleet',
-	 'snow' : 'snow',
-	 'sunny' : 'sunny',
-	 'tstorms' : 'tstorm',
-	 'unknown' : '???'}
+icons = {
+'chanceflurries': 'flurry?',
+'chancerain': 'rain?',
+'chancesleet': 'sleet?',
+'chancesnow': 'snow?',
+'chancetstorms': 'tstorm?',
+'clear': 'clear',
+'cloudy': 'cloudy',
+'flurries': 'flurry',
+'fog': 'fog',
+'hazy': 'hazy',
+'mostlycloudy': 'cloudy',
+'mostlysunny': 'sunny',
+'partlycloudy': 'cloudy~',
+'partlysunny': 'sunny~',
+'rain': 'rain',
+'sleet': 'sleet',
+'snow': 'snow',
+'sunny': 'sunny',
+'tstorms': 'tstorm',
+'unknown': '???'
+}
 
 # Print current data
 temp = str(current['temp_f'])
@@ -42,4 +44,4 @@ wspd = str(current['wind_mph'])
 wgus = str(current['wind_gust_mph'])
 wdir = str(current['wind_degrees'])
 cond = icons[str(current['icon'])]
-print '{:s} | {:s} ({:s}) @ {:s} | {:s}'.format(temp,wspd,wgus,wdir,cond)
+print '{:s} | {:s} ({:s}) @ {:s} | {:s}'.format(temp, wspd, wgus, wdir, cond)
