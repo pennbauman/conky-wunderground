e2f873ae732f3141068d5d5aaa72cfbcf49f814a
Isaac Velando <iwvelando@gmail.com>
2013-10-27 10:19:49 -0500

    Added a runner that doesn't invoke conky for piping into a bar

---
+++ runner_wunderground_min.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Try and make sure the internet is connected
+sleep 10
+
+/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042 > /tmp/current-forecast-$USER &
+curl -m 60 http://ipecho.net/plain -o /tmp/pub-ip-$USER &
+
+while true; do
+	sleep 600
+	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042 > /tmp/current-forecast-$USER &
+	if [ ! -e /tmp/pub-ip-$USER ]; then
+		curl -m 60 http://ipecho.net/plain -o /tmp/pub-ip-$USER &
+	fi
+done
