8678da132d52c79c60e3ed431101422316eeba89
Isaac Velando <iwvelando@gmail.com>
2013-10-29 18:49:33 -0500

    Basic error handling for weather call

--- runner_wunderground_desktop.sh
+++ runner_wunderground_desktop.sh
@@ -3,16 +3,28 @@
 # Try and make sure the internet is connected
 sleep 10
 
-/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7 > /tmp/daily-forecast-$USER &
-/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17 > /tmp/hourly-forecast-$USER &
+daily=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7)
+if [ $? == 0 ]; then
+	echo $daily > /tmp/daily-forecast-$USER
+fi
+hourly=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17)
+if [ $? == 0 ]; then
+	echo $hourly > /tmp/hourly-forecast-$USER
+fi
 $HOME/git/conky_wunderground_scripts/pubip.sh &
 
 sleep 10 && conky -dq &
 
 while true; do
 	sleep 600
-	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7 > /tmp/daily-forecast-$USER &
-	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17 > /tmp/hourly-forecast-$USER &
+	daily=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7)
+	if [ $? == 0 ]; then
+		echo $daily > /tmp/daily-forecast-$USER
+	fi
+	hourly=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17)
+	if [ $? == 0 ]; then
+		echo $hourly > /tmp/hourly-forecast-$USER
+	fi
 	if [ ! -e /tmp/pub-ip-$USER ]; then
 		$HOME/git/conky_wunderground_scripts/pubip.sh &
 	fi
