a295c2f94ca933489398a1b83fd5c0955375ea78
Isaac Velando <iwvelando@gmail.com>
2013-10-29 18:46:20 -0500

    Basic error handling for weather call

--- runner_wunderground_min.sh
+++ runner_wunderground_min.sh
@@ -3,12 +3,18 @@
 # Try and make sure the internet is connected
 sleep 10
 
-/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042 > /tmp/current-forecast-$USER &
+weather=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042)
+if [ $? == 0 ]; then
+	echo $weather > /tmp/current-forecast-$USER
+fi
 $HOME/git/conky_wunderground_scripts/pubip.sh &
 
 while true; do
 	sleep 600
-	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042 > /tmp/current-forecast-$USER &
+	weather=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_current.py 77042)
+	if [ $? == 0 ]; then
+		echo $weather > /tmp/current-forecast-$USER
+	fi
 	if [ ! -e /tmp/pub-ip-$USER ]; then
 		$HOME/git/conky_wunderground_scripts/pubip.sh &
 	fi
