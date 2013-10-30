3b42a7615130578facee9f09de5c807aae6d4973
Isaac Velando <iwvelando@gmail.com>
2013-10-29 19:06:45 -0500

    Reverted change due to formatting issues

--- runner_wunderground_desktop.sh
+++ runner_wunderground_desktop.sh
@@ -3,28 +3,16 @@
 # Try and make sure the internet is connected
 sleep 10
 
-daily=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7)
-if [ $? == 0 ]; then
-	echo $daily > /tmp/daily-forecast-$USER
-fi
-hourly=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17)
-if [ $? == 0 ]; then
-	echo $hourly > /tmp/hourly-forecast-$USER
-fi
+/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7 > /tmp/daily-forecast-$USER &
+/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17 > /tmp/hourly-forecast-$USER &
 $HOME/git/conky_wunderground_scripts/pubip.sh &
 
 sleep 10 && conky -dq &
 
 while true; do
 	sleep 600
-	daily=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7)
-	if [ $? == 0 ]; then
-		echo $daily > /tmp/daily-forecast-$USER
-	fi
-	hourly=$(/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17)
-	if [ $? == 0 ]; then
-		echo $hourly > /tmp/hourly-forecast-$USER
-	fi
+	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_daily_forecast.py 77070 7 > /tmp/daily-forecast-$USER &
+	/usr/bin/python2 $HOME/git/conky_wunderground_scripts/wunderground_hourly_forecast.py 77070 17 > /tmp/hourly-forecast-$USER &
 	if [ ! -e /tmp/pub-ip-$USER ]; then
 		$HOME/git/conky_wunderground_scripts/pubip.sh &
 	fi
