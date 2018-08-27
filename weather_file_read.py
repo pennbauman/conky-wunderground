3885ad808e3c911e49fb2fe0d1f84b4aca933983
Penn Bauman <penn.bauman@gmail.com>
2018-08-27 18:27:11 -0400

    further cleanup

--- weather_file_read.py
+++ weather_file_read.py
@@ -2,8 +2,7 @@ import sys
 
 # Get data number
 item = sys.argv[1]
-item = int(item)
-item += 2
+item = int(item) + 3
 # Read data from weather_save.txt
 data = open(sys.path[0] + "/weather_save.txt", "r")
 data = data.read().split('\n')[item]
--- wunderground_weather_get.py
+++ wunderground_weather_get.py
@@ -4,34 +4,30 @@ import sys
 import os
 import datetime
 
-# Time
+# Get time
 now = datetime.datetime.now()
-
-# Extract command line arguments and grab the wunderground API key
-location = sys.argv[1]
+# Get zip code
+zipCode = sys.argv[1]
+# Get apiKey
 apiKey = open(os.getenv('HOME') + '/.api/wunderground', 'r').readline().split('\n')[0]
-
-# Wunderground API call for current conditions data
-f = urllib2.urlopen('http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + location + '.json')
-json_string = f.read()
-f.close()
-parsed_json = json.loads(json_string)
-current = parsed_json['current_observation']
-
-# Wunderground API call for 10 day forecast data
-f = urllib2.urlopen('http://api.wunderground.com/api/' + apiKey + '/forecast10day/q/' + location + '.json')
-json_string = f.read()
-f.close()
-parsed_json = json.loads(json_string)
-forecast = parsed_json['forecast']['simpleforecast']['forecastday']
-
+# Get current weather data
+urlFile = urllib2.urlopen('http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + zipCode + '.json')
+jsonString = urlFile.read()
+urlFile.close()
+jsonParsed = json.loads(jsonString)
+weatherNow = jsonParsed['current_observation']
+# Get 10 day forcast data
+urlFile = urllib2.urlopen('http://api.wunderground.com/api/' + apiKey + '/forecast/q/' + zipCode + '.json')
+jsonString = urlFile.read()
+urlFile.close()
+jsonParsed = json.loads(jsonString)
+weatherForecast = jsonParsed['forecast']['simpleforecast']['forecastday']
 # Convert weather codes to readable
 icons = {'chanceflurries' : 'Chance of Flurries',
 	'chancerain' : 'Chance of Rain',
 	'chancesleet' : 'Chance of Sleet',
 	'chancesnow' : 'Chance of Snow',
 	'chancetstorms' : 'Chance of Thunderstorms',
-	
 	'clear' : 'Clear',
 	'cloudy' : 'Cloudy',
 	'flurries' : 'Flurries',
@@ -48,31 +44,30 @@ icons = {'chanceflurries' : 'Chance of Flurries',
 	'tstorms' : 'Thunderstorming',
 	'unknown' : 'unknown'
 }
+# Assemble all relivant data
+lines = [
+	"Time: " + now.strftime("%Y-%m-%d %H:%M:%S%z"), #timestamp
+	"Zip:  " + zipCode,
+	"",
+	str(weatherNow['temp_f']), # 0 current temp
+	icons[str(weatherNow['icon'])], # 1 current cond
+	
+	str(weatherForecast[0]['low']['fahrenheit']), # 2 today's expected low temp
+	str(weatherForecast[0]['high']['fahrenheit']), # 3 today's expected high temp
+	icons[str(weatherForecast[0]['icon'])], # 4 today's expected cond
+	str(weatherForecast[0]['pop']), # 5 today's expected rain chance (%)
+	str(weatherForecast[0]['qpf_allday']['in']), # 6 today's expected rain fall (in.)
 
-cond = icons[str(current['icon'])]
-cond = "%s%s" % (cond[0].upper(), cond[1:])
-
+	str(weatherForecast[1]['low']['fahrenheit']), # 7 tomorrow's expected low temp
+	str(weatherForecast[1]['high']['fahrenheit']), # 8 tomorrow's expected high temp
+	icons[str(weatherForecast[1]['icon'])], # 9 tomorrow's expected cond
+	str(weatherForecast[1]['pop']), # 10 tomorrow's expected rain chance (%)
+	str(weatherForecast[1]['qpf_allday']['in']), # 11 tomorrow's expected rain fall (in.)	
+]
+saveText = ""
+for line in lines:
+	saveText = saveText + line + "\n"
 # Write data to weather.txt
 file = open(sys.path[0] + "/weather_save.txt", "w")
-lines = [
-	now.strftime("%Y-%m-%d %H:%M"), #timestamp
-	"",
-	str(current['temp_f']), # 0 current temp
-	cond, # 1 current cond
-	str(forecast[0]['low']['fahrenheit']), # 2 today's expected low temp
-	str(forecast[0]['high']['fahrenheit']), # 3 today's expected high temp
-	icons[str(forecast[0]['icon'])], # 4 today's expected cond
-	str(forecast[0]['pop']), # 5 today's expected rain chance (%)
-	str(forecast[0]['qpf_allday']['in']), # 6 today's expected rain fall (in.)
-
-	str(forecast[1]['low']['fahrenheit']), # 7 tomorrow's expected low temp
-	str(forecast[1]['high']['fahrenheit']), # 8 tomorrow's expected high temp
-	icons[str(forecast[1]['icon'])], # 9 tomorrow's expected cond
-	str(forecast[1]['pop']), # 10 tomorrow's expected rain chance (%)
-	str(forecast[1]['qpf_allday']['in']), # 11 tomorrow's expected rain fall (in.)	
-]
-text = ""
-for line in lines:
-	text = text + line + "\n"
-file.write(text)
+file.write(saveText)
 file.close()
