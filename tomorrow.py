5bd9274431bfdddb53a253cbc00c8b05906c7b17
Penn Bauman <penn.bauman@gmail.com>
2018-08-27 18:32:54 -0400

    commented tommorow.py

--- tomorrow.py
+++ tomorrow.py
@@ -1,11 +1,14 @@
 import datetime
 
+# Get todays weekday number
 day_num = datetime.datetime.today().weekday()
 day_num = day_num + 1
 
+# Switch if past Sunday
 if day_num == 7:
 	day_num = 0
 
+# Convert number to text
 num_text = {
 	0:"Monday", 
 	1:"Tuesday", 
@@ -17,4 +20,5 @@ num_text = {
 }
 day_text = num_text[day_num]
 
+# Output Day
 print day_text
